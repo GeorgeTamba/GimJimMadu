42a67a65f74b0465311bf6f3b64116a568d36baa
GeorgeTamba <georgelvertamba@gmail.com>
2025-02-18 19:44:05 +0700

    Player Interaction & Interactable Items

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+
+    public float playerReach = 3f;
+    Interactable currentInteractable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInteraction();
+        if(Input.GetKeyDown(KeyCode.F) && currentInteractable != null)
+        {
+            currentInteractable.Interact();
+        }
+    }
+
+    void CheckInteraction()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);   
+
+        //if colliders with anything within player reach
+        if(Physics.Raycast(ray, out hit, playerReach))
+        {
+            if(hit.collider.tag == "Interactable")//if looking at an interactable object
+            {
+                Interactable newInteractable = hit.collider.GetComponent<Interactable>();
+
+                if(currentInteractable && newInteractable != currentInteractable)
+                {
+                    currentInteractable.DisableOutline();
+                }
+                if (newInteractable.enabled)
+                {
+                    SetNewCurrentInteractable(newInteractable);
+                }
+            }
+            else //if not an interactable
+            {
+                DisableCurrentInteractable();
+            }
+        }
+        else //if nothing in reach
+        {
+            DisableCurrentInteractable();   
+        }
+    }
+
+    void SetNewCurrentInteractable(Interactable newInteractable)
+    {
+        currentInteractable = newInteractable;
+        currentInteractable.EnableOutline();
+    }
+
+    void DisableCurrentInteractable()
+    {
+        if (currentInteractable)
+        {
+            currentInteractable.DisableOutline();
+            currentInteractable = null;
+        }
+    }
+
+}
